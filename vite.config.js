727d120b220f741ce03264af9416dd54eaa696d2
Robin Warden <robincw35@gmail.com>
2025-05-20 16:46:37 -0400

    Modified vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        tech: resolve(__dirname, 'tech.html'),
+        work: resolve(__dirname, 'work.html'),
+        // Add other HTML files here as needed
+      }
+    }
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'vercel.json',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   server: {
     open: true
   }
-})
+}); 
+
